1171316f50bb717694e24c270ce999036a00b0aa
Abeleshev <abeleshev1993@gmail.com>
2022-06-01 17:13:39 +0300

    create structure db tables for namecheap

--- model/model.js
+++ model/model.js
@@ -115,3 +115,90 @@ const domainsDataGodaddy = Sequelize.define('domainsDataGodaddy', {
 accountGodaddy.hasMany(domainsDataGodaddy);
 domainsDataGodaddy.belongsTo(accountGodaddy);
 
+const accountNamecheap = Sequelize.define('accountNamecheap',
+{
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+
+    name: {
+        type: DataTypes.STRING,
+        unique: false,
+        allowNull:false
+    },
+    
+    login: {
+        type: DataTypes.STRING,
+        unique: false,
+        allowNull:false
+    },
+
+    password:{
+        type: DataTypes.STRING,
+        unique: false,
+        allowNull:false
+    },
+
+    key:{
+        type: DataTypes.STRING,
+        unique: false,
+        allowNull:false
+    }
+});
+
+const domainsNamecheap = Sequelize.define('domainsNamecheap', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    idDomen: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    user: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    created: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    expires: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    isExpired: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    isLocked: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    autoRenew: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    whoisGuard: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    isPremium: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    isOurDNS: {
+        type: DataTypes.false,
+        allowNull: false
+    }
+});
+
+accountNamecheap.hasMany(domainsNamecheap);
+domainsNamecheap.belongsTo(accountNamecheap);
